b8afe497eda5f0f06357c6ec0d522a28c4270b3c
swdecker <swdecker@gmail.com>
2019-11-26 11:35:18 -0600

    added sort and filter functionality

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -2,12 +2,15 @@ import React from "react";
 import BotCollection from './BotCollection'
 import YourBotArmy from './YourBotArmy'
 import BotSpecs from '../components/BotSpecs'
+import BotFilter from './BotFilter'
 class BotsPage extends React.Component {
   //start here with your code for step one
   state = {
     bots:[],
     botArmy:[],
-    inspectBot: null
+    inspectBot: null,
+    filter:"All",
+    sort:"health"
   }
 
   componentDidMount=()=>{
@@ -47,15 +50,55 @@ class BotsPage extends React.Component {
     })
   }
 
+  handleFilterChange=(event)=>{
+    this.setState({
+      filter:event.target.value
+    })
+  }
+  handleSortChange=(event)=>{
+    this.setState({
+      sort:event.target.value
+    })
+  }
+
+  filterBots=()=>{
+    if(this.state.filter==="All"){
+      return this.sortBots()
+    } else {
+      return this.sortBots().filter(bot=>bot.bot_class===this.state.filter)
+    }
+  }
+
+  sortBots=()=>{
+    if(this.state.sort !== 'sum'){
+      return this.state.bots.sort((bot1,bot2)=>{
+        if(bot1[this.state.sort]<bot2[this.state.sort]){
+          return 1
+        } else {
+          return -1
+        }
+      })
+    } else {
+      // sort is sum
+      return this.state.bots.sort((bot1,bot2)=>{
+        if((bot1.armor+bot1.damage+bot1.health)>(bot2.armor+bot2.damage+bot2.health)){
+          return 1
+        } else {
+          return -1
+        }
+      })
+    }
+  }
 
 
   render() {
     return (
       <div>
         <YourBotArmy handleRemoveBot={this.handleRemoveBot} botArmy={this.state.botArmy}/>
+        <BotFilter handleSortChange={this.handleSortChange} sort={this.state.sort} handleFilterChange={this.handleFilterChange} filter={this.state.filter}/>
         { this.state.inspectBot ?
           <BotSpecs bot={ this.state.inspectBot } handleAddBot={this.handleAddBot} handleClearInspect={this.handleClearInspect}/> :
-          <BotCollection handleInspectBot={this.handleInspectBot} bots={this.state.bots} />
+          <BotCollection handleInspectBot={this.handleInspectBot} bots={this.filterBots()} />
         }
       </div>
     );
